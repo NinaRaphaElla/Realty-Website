b351d26acea79666da38ebe14c6f88e55b4b000f
NinaRaphaElla <velasco.ninaraphaella.s@gmail.com>
2024-04-03 14:08:10 +0800

    About us changes

--- Realty-app/src/App.jsx
+++ Realty-app/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Header from "./Components/Header/Header.jsx";
 import Home from "./Components/Home/Home.jsx";
+import AboutUs from "./Components/AboutUs/AboutUs.jsx";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Home />
+      {/* <Home /> */}
       <AboutUs />
     </>
   );
--- Realty-app/src/Components/AboutUs/AboutUs.jsx
+++ Realty-app/src/Components/AboutUs/AboutUs.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AboutUs = () => {
-  result(
+  return (
     <>
       <div>Code here</div>
     </>
