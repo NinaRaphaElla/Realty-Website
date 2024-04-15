62825714c1e6c2a63672a9be0c3630f52149dfe5
NinaRaphaElla <velasco.ninaraphaella.s@gmail.com>
2024-04-15 16:14:37 +0800

    feat ScrollToTop feat PropertyView fix route and link

--- Realty-app/src/Components/PropertyCard/PropertyCard.jsx
+++ Realty-app/src/Components/PropertyCard/PropertyCard.jsx
@@ -1,68 +1,73 @@
-import propTypes from "prop-types";
 import TextButton from "../Buttons/TextButton";
-import propertyImage from "./property-img.png";
-import propertyList from "/src/propertyList.json";
+import propertyList from "./propertyList.json";
+import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const PropertyCard = (props) => {
+const PropertyCard = () => {
+  const history = useHistory();
+
+  // const handleViewMore = () => {
+  //   // Navigate to the about us page when the button is clicked
+  //   history.push("/details");
+  // };
+
   return (
     <>
-      <div className="hover:-translate-y-2 transition duration-300 ease-in-out">
-        <div className="bg-white flex flex-col max-w-80 h-[460px]">
+      {propertyList.map((list) => (
+        <div
+          className="bg-white flex flex-col max-w-80 h-[450px] hover:-translate-y-2 transition-all ease-in-out duration-200"
+          key={list.id}
+        >
           <div className="relative">
             <img
               className="max-w-full relative"
               alt="Property image"
-              src={propertyImage}
+              src={list.propertyImg}
             />
             <p className="absolute m-0 top-0 bg-primary text-white text-base py-1 px-6">
-              Featured
+              {list.isFeatured === true ? "Featured" : list.status}
             </p>
           </div>
 
           <div className="text-black p-4">
-            <h1 className="m-0 mb-2 text-2xl">{props.title}</h1>
-            <p className="m-0 mb-2 text-gray text-xs font-sans">
-              {props.description}
+            <h1 className="mb-2 text-xl h-[25%]">{list.title}</h1>
+            <p className="mb-2 text-gray text-xs font-sans">
+              {list.description}
             </p>
-            <p className="m-0 mb-2 text-2xl font-sans font-semibold">
-              &#8369;{props.price}
+            <p className="m-0 mb-2 text-xl font-sans font-semibold">
+              &#8369;{list.price}
             </p>
 
             {/* property overview div */}
-            <div className="flex justify-between font-sans text-xs mb-6 font-medium text-center text-gray">
-              <div>
-                <p>Bedrooms</p>
-                <p>{props.bedQuantity}</p>
+            {list.overview.map((overviewList) => (
+              <div className="flex justify-between font-sans text-xs mb-6 font-medium text-center text-gray">
+                <div>
+                  <p>Bedrooms</p>
+                  <p>{overviewList.bedroom}</p>
+                </div>
+                <div>
+                  <p>Bathrooms</p>
+                  <p>{overviewList.bathroom}</p>
+                </div>
+                <div>
+                  <p>Year Built</p>
+                  <p>{overviewList.yearBuilt}</p>
+                </div>
+                <div>
+                  <p>Land Area</p>
+                  <p>{overviewList.area} sqft</p>
+                </div>
               </div>
-              <div>
-                <p>Bathrooms</p>
-                <p>{props.bathQuantity}</p>
-              </div>
-              <div>
-                <p>Year Built</p>
-                <p>{props.yearBuilt}</p>
-              </div>
-              <div>
-                <p>Land Area</p>
-                <p>{props.landArea} sqft</p>
-              </div>
-            </div>
-            <TextButton btnName={"View more"} className="bg-red-100" />
+            ))}
+            {/* <TextButton btnName={"View more"} onClick={handleViewMore} /> */}
+            <Link to={`/property-details/${list.id}`}>
+              <TextButton btnName={"View more"} />
+            </Link>
           </div>
         </div>
-      </div>
+      ))}
     </>
   );
-};
-
-PropertyCard.propTypes = {
-  title: propTypes.string,
-  description: propTypes.string,
-  price: propTypes.number,
-  bedQuantity: propTypes.number,
-  bathQuantity: propTypes.number,
-  yearBuilt: propTypes.number,
-  landArea: propTypes.number,
 };
 
 export default PropertyCard;
