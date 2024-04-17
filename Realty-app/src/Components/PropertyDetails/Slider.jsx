b90aca879035cb8dac761ce9e802c22ebab4104a
AlyssaJoyDR <alyssajoydelrosario3@gmail.com>
2024-04-17 17:27:14 +0800

    fix property details responsiveness, add component for property details

--- Realty-app/src/Components/PropertyDetails/Slider.jsx
+++ Realty-app/src/Components/PropertyDetails/Slider.jsx
@@ -5,19 +5,24 @@ function SliderImg() {
 
     const slides = [
         {
-            url: 'https://images.unsplash.com/photo-1712928247899-2932f4c7dea3?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            url: 'https://images.unsplash.com/photo-1560184897-67f4a3f9a7fa?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            label: 'Living Room'
         },
         {
-            url: 'https://media.istockphoto.com/id/1655115306/photo/interior-wall-mockup.jpg?s=612x612&w=0&k=20&c=PS4eAiFb3YPJGKmMlcnE5A9ddWhwruVXl2q4mP-DsAc='
+            url: 'https://images.unsplash.com/flagged/photo-1573168710865-2e4c680d921a?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            label: 'Bed Room'
         },
         {
-            url: 'https://media.istockphoto.com/id/1346607516/photo/cabinet-in-living-room-with-tatami-mat-floor-and-sofa-armchair-design-3d-rendering.jpg?s=612x612&w=0&k=20&c=26uISiVtsmLmFBLXp5C3sJNGUVjClgaq83Mnc5GasJU='
+            url: 'https://plus.unsplash.com/premium_photo-1682377521566-f70ac8abe597?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            label: 'Kitchen Area'
         },
         {
-            url: 'https://media.istockphoto.com/id/478197982/photo/luxury-house-interior-bright-elegant-dining-area.jpg?s=2048x2048&w=is&k=20&c=YXq13d-ceoGpKjiUIwQwjZ1yjCAfUoyQ8jSdXh5Ecxc='
+            url: 'https://images.unsplash.com/photo-1631048499052-e6d9f305d2c0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            label: 'Bathroom'
         },
         {
-            url: 'https://media.istockphoto.com/id/1824615199/photo/orange-sofa-in-cozy-living-room-interior-with-pastel-green-wall-and-wood-furniture-wall.jpg?s=612x612&w=0&k=20&c=ysyQe6R17y6nh9qc4-CghkfwxsOuS-SPBsW-P7Jmzwg='
+            url: 'https://st.hzcdn.com/simgs/pictures/sheds/stonehaven-garage-blue-sky-building-company-img~2d01fa340ef24f03_14-2817-1-3b7d911.jpg',
+            label: 'Garage'
         },
     ];
 
@@ -30,24 +35,46 @@ function SliderImg() {
     };
 
     const nextSlide = () => {
-        const isLastSlide = currentIndex === 0;
+        const isLastSlide = currentIndex === slides.length - 1;
         const newIndex = isLastSlide ? 0 : currentIndex + 1;
         setCurrentIndex(newIndex);
     };
 
+    const goToSlide = (slideIndex) => {
+        setCurrentIndex(slideIndex);
+    };
+
     return (
-    <div className='max-w-[1400px] h-[650px] w-full m-auto py-24 px-4 relative group'>
-        <div
-            style={{ backgroundImage: `url(${slides[currentIndex].url})`}} 
-            className='w-full h-full bg-center bg-cover duration-500'>
+    <div className='max-w-[1420px] h-[480px] lg:h-[680px] mx-2 md:mx-14 m-auto mt-4 py-24 px-2 relative group transition-all ease-in-out duration-300'>
+            <div className='absolute w-[97%] md:w-[99%] text-md md:text-xl text-rawWhite bottom-28 flex justify-center items-center'>
+                <span style={{
+                    textAlign: 'center',
+                    maxWidth: '90%',
+                    overflowWrap: 'break-word' }}>
+                {slides[currentIndex].label}
+                </span>
+            </div>
+            <div
+                style={{ backgroundImage: `url(${slides[currentIndex].url})`}}
+                className='w-full h-full bg-center bg-cover duration-500'>
             </div>
     {/* Left Arrow */}
-        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl text-white cursor-pointer'>
-            <MdKeyboardArrowLeft onClick={prevSlide} size={30} />
+        <div className='md:hidden md:group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-8 text-2xl rounded-full shadow bg-accent text-white cursor-pointer'>
+            <MdKeyboardArrowLeft onClick={prevSlide} size={40} />
         </div>
     {/* Right Arrow */}
-        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl text-white cursor-pointer'>
-            <MdKeyboardArrowRight onClick={nextSlide} size={30}/>
+        <div className='md:hidden md:group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-8 text-2xl rounded-full shadow bg-accent text-white cursor-pointer'>
+            <MdKeyboardArrowRight onClick={nextSlide} size={40}/>
+        </div>
+
+        <div className='flex top-10 items-center justify-center py-4 gap-1 md:gap-2'>
+            {slides.map((_, i) => (
+                <div className={`transition-all w-1.5 h-1.5 md:w-2 md:h-2 bg-accent rounded-full
+                ${currentIndex === i ? "transition-all ease-in-out duration-300 p-0.5 md:p-1" : "bg-opacity-40"}
+                `}
+                />
+                
+            ))}
         </div>
     </div>
 );
